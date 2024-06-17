933777b8f154639502731a6de158fdbe65bb4f2e
hungdt1509 <111280504+HungDang123@users.noreply.github.com>
2024-06-17 12:26:36 +0700

    update product repository

--- src/main/java/com/hominhnhut/WMN_BackEnd/repository/ProductRepository.java
+++ src/main/java/com/hominhnhut/WMN_BackEnd/repository/ProductRepository.java
@@ -5,10 +5,16 @@ import com.hominhnhut.WMN_BackEnd.domain.enity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ProductRepository extends JpaRepository<Product, String> {
 
     Page<Product> findAllByProductNameContaining(String usernameContain, Pageable pageable);
 
     Product getProductByProductId(String s);
+    @Query(value = "SELECT p FROM Product p " +
+            "INNER JOIN p.category c ON p.category.categoryId = c.categoryId \n" +
+            "WHERE c.categoryId = :categoryId")
+    Page<Product> getProductByCategoryId(@Param("categoryId") String categoryId, Pageable pageable);
 }
